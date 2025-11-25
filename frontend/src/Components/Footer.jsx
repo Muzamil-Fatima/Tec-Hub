bc89460c2a40aa43bbb03653458ee82355eda62a
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2025-11-25 17:28:08 +0500

    Add Components

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <h2>TecHub</h2>
+        <p>Address xyz.</p>
+        <p>TecHub@gmail.com</p>
+        <p>+92 xxxxxxxxxxx</p>
+      </div>
+      <div>
+        <div>
+          <h3>Quick Link</h3>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Internship</li>
+            <li>Contact</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Legal</h3>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Terms & Condition</li>
+            <li>Refund & Cancellation</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Trusted References</h3>
+          <ul>
+            <li>HEC</li>
+            <li>Digital Pakistan</li>
+            <li>XYZ</li>
+            <li>XYZ</li>
+            <li>XYZ</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h6>NewsLetter</h6>
+        <p>
+          Subscribe to get the latest updates, offers, and news from Syntecxhub.
+        </p>
+        <div>
+          <input type="text" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+      <div>
+        <p>Follow Us</p>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <p>@2025 Muzamil-Fatima. All right reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
