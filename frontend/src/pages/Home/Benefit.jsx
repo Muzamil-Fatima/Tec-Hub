96f94e0c59356cc060ac7ff0a511164402571d35
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2025-11-26 16:00:43 +0500

    add more css

--- frontend/src/pages/Home/Benefit.jsx
+++ frontend/src/pages/Home/Benefit.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Benefit = () => {
   return (
-    <div className="flex flex-col gap-2.5 items-center p-4 w-lg pt-8 px-6 hover:shadow-2xl hover:shadow-gray-300 pb-8 rounded-lg">
+    <div className="flex flex-col gap-2.5 items-center p-4 w-lg pt-8 px-6 shadow-2xl shadow-gray-300 pb-8 rounded-lg hover:shadow-xl">
       {/* <img src="" alt="" /> */}
-      <p>MMMM</p>
+      <p className="bg-linear-to-br from-purple-300 via-purple to bg-pink-300 rounded-full w-18 h-18 flex justify-center items-center hover:rotate-45 ">MMMM</p>
       <p className="font-bold text-2xl">Skill Development</p>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat ad
