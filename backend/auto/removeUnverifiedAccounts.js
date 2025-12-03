1fe7c5495a21f99dbf68b5fa40c822bd8a4b94de
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2025-12-03 16:49:33 +0500

    Add dependency

---
+++ backend/auto/removeUnverifiedAccounts.js
@@ -0,0 +1,12 @@
+import corn from "node-cron";
+import User from "../models/User.js";
+
+export const removeUnverifiedAccount = () => {
+  corn.schedule("*30 * * * *", async () => {
+    const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+    await User.deleteMany({
+      accountVerified: false,
+      createdAt: { $lt: thirtyMinutesAgo },
+    });
+  });
+};
---
+++ backend/router/userRoutes.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import mongooseSequence from "mongoose-sequence";
+import bcrypt from "bcrypt.js";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
