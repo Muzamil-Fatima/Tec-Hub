117eeac4620e730f7852157ac643d9958c9ac734
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2025-12-13 12:44:21 +0500

    logout endpoint test sucessfully

--- backend/router/authRoutes.js
+++ backend/router/authRoutes.js
@@ -1,11 +1,16 @@
 // authRoutes.js
 import express from "express";
-import { signup, login } from "../controllers/authController.js";
+import {
+  signup,
+  login,
+  logoutCurrentUser,
+} from "../controllers/authController.js";
 const router = express.Router();
 
 // routes
 router.post("/", signup);
 router.post("/login", login);
+router.get("/logout", logoutCurrentUser);
 
 // your routes here
 export default router;
